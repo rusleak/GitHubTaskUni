f93a844ea798976b59e8f2e63d9519770171dc39
rusleak <hotlinetony2@gmail.com>
2025-04-04 10:11:59 +0200

    Person/Cat/Dog replaced to Person/MyMult/MySum 04.04.2025

---
+++ MyMult.py
@@ -0,0 +1,6 @@
+class MyMult:
+    @staticmethod
+    def myMult(num,num1):
+        print(num*num1)
+MyMult.myMult(3,5)
+
---
+++ MySum.py
@@ -0,0 +1,6 @@
+class MySum:
+    @staticmethod
+    def mySum(num,num1):
+        print(num+num1)
+MySum.mySum(3,5)
+
